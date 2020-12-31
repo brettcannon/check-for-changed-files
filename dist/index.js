239f9f7dfc437872b97cbc8be526b5c168d4678a
Brett Cannon <brett@python.org>
2020-12-31 10:26:42 -0800

    Have failure message log the prereq pattern (#7)

--- dist/index.js
+++ dist/index.js
@@ -7142,27 +7142,27 @@ async function run() {
     try {
         const payload = gh.pullRequestPayload();
         if (payload === undefined) {
-            core.info(`the event '${github.context.eventName}' is not for a pull request; skipping`);
+            core.info(`'${github.context.eventName}' is not a pull request event; skipping`);
             return;
         }
         const skipLabel = core.getInput("skip-label");
         const prLabels = gh.pullRequestLabels(payload);
         if (matching.hasLabelMatch(prLabels, skipLabel)) {
-            core.info(`the skip label '${skipLabel}' matched`);
+            core.info(`the skip label '${skipLabel}' is set`);
             return;
         }
         const filePaths = await gh.changedFiles(payload);
         const prereqPattern = core.getInput("prereq-pattern") || matching.defaultPrereqPattern;
         if (!matching.anyFileMatches(filePaths, prereqPattern)) {
-            core.info(`prerequisite glob pattern '${prereqPattern}' did not match any changed files`);
+            core.info(`prerequisite '${prereqPattern}' did not match any changed files`);
             return;
         }
         const filePattern = core.getInput("file-pattern", { required: true });
-        if (!matching.anyFileMatches(filePaths, filePattern)) {
-            core.setFailed(`the glob pattern '${filePattern}' did not match any changed files`);
+        if (matching.anyFileMatches(filePaths, filePattern)) {
+            core.info(`'${filePattern}' matched one of the changed files`);
             return;
         }
-        core.info(`the glob pattern '${filePattern}' matched one of the changed files`);
+        core.setFailed(`prerequisite '${prereqPattern}' matched, but '${filePattern}' did NOT match any changed files`);
     }
     catch (error) {
         core.setFailed(error.message);
