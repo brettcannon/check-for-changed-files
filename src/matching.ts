39c008b36caabe0ea7c26ffe4591868c1cc85e2a
Brett Cannon <brett@python.org>
2020-12-31 09:53:59 -0800

    Add anyFileMatches()

--- src/matching.ts
+++ src/matching.ts
@@ -1,5 +1,13 @@
 import * as minimatch from "minimatch";
 
+function anyFileMatches(
+  filePaths: readonly string[],
+  pattern: string
+): boolean {
+  const regexp = minimatch.makeRe(pattern);
+  return filePaths.some((val) => regexp.test(val));
+}
+
 /**
  * Check if any of the file paths match the file glob pattern.
  */
